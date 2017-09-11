46a0f91f8854dca6f920b78202bbe18cbf4c79b3
seuxxd <894873161@qq.com>
2017-09-11 15:45:39 +0800

    2017-09-11 第三次更新

--- app/src/main/java/com/product/xxd/latestliftapplication/LoginActivity.java
+++ app/src/main/java/com/product/xxd/latestliftapplication/LoginActivity.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindString;
 import butterknife.BindView;
@@ -33,6 +37,7 @@ public class LoginActivity extends AppCompatActivity {
 //    工具栏的引用
     @BindView(R.id.login_toolbar)
     Toolbar mToolbar;
+//    Toolbar里面的标题自定义的文本框
     @BindView(R.id.toolbar_title)
     TextView mToolbarTitle;
 //    字符串的绑定
@@ -46,9 +51,40 @@ public class LoginActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         mUsernameTextInput.setHint("Username");
         mPasswordTextInput.setHint("Password");
-//        mToolbar.setTitle(mTitle);
+        mToolbar.setTitle("");
         mToolbarTitle.setText(mTitleString);
+        /*mToolbar.inflateMenu(R.menu.login_menu);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.app_usage:
+                        Toast.makeText(LoginActivity.this, "用法", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+            }
+        });*/
         setSupportActionBar(mToolbar);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.login_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.app_usage:
+                Toast.makeText(this, "APP_USAGE", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
